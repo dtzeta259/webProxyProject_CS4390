0bfd33969c345d9cd71c50928f25b02c3f41df80
dtzeta259 <catalystoftime259@gmail.com>
2022-04-22 14:11:41 -0500

    http parsing and caching done by Duy Vu

--- WebProxyServer.py
+++ WebProxyServer.py
@@ -10,8 +10,9 @@ import re
 
 #Variables for TCP sockets
 BUFFER_SIZE = 6144
-PORT = 65003 #The port that will be used for the project
-SERVER = socket.gethostbyname(socket.gethostname())
+PORT = 5005 #The port that will be used for the project
+#SERVER = socket.gethostbyname(socket.gethostname())
+SERVER = "localhost"
 SERVER_ADDR = (SERVER, PORT)
 
 #Read in the arguments for the server ip and port, if any
@@ -28,25 +29,131 @@ parser.add_argument("serverPort", nargs="?", type=int, default=PORT,
 argServer = parser.parse_args()
 
 #Methods for the proxy server
-
+"""
 def requestsGET(clientSock, address):
-    print("Message Received from Client {}:\n" .format(address))
-    
+	print("Message Received from Client {}:\n" .format(address))
     clientMessage = clientSock.recv(BUFFER_SIZE).decode()
-    
     #Parse the HTTP request and print it out
-    getRequest = clientMessage.split()
-
+	getRequest = clientMessage.split()
+"""
 #Create proxy server socket and listen for get requests
 serverTCP = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 serverTCP.bind(SERVER_ADDR)
 serverTCP.listen(5)
 print("HTTP Proxy Server is listening on IP and port: {}:{}" .format(argServer.serverIP, argServer.serverPort))
-
 #Connection is established, begin serving via threading
-while True:
-    clientSocket, address = serverTCP.accept()
-    print("Connection from {} established! Begin client communication." .format(address))
-    threading.Thread(target=requestsGET, args=(clientSocket, address))
-    
+def get_header(response):
+	res = response
+	res = res.split('\r\n\r\n')[0]
+	return res
+def parse_data(response):
+	#return method, destAddr, http version
+	cur = ""
+	lst = []
+	for i in range(len(response)):
+		char = response[i]
+		if (char == '\r' or char == '\n'):
+			lst.append(cur)
+			break
+		elif (char == ' '):
+			lst.append(cur)
+			cur = ""
+		else:
+			cur += char
+	return lst[0] , lst[1][1:] , lst[2]
 
+def parse_link(link):
+	#need to get host , url , filename
+	host = ""
+	for i in range(len(link)):
+		if (link[i] == str('/')[0]):
+			break
+		host += link[i]
+	filename = ""
+	for i in range(len(link)):
+		if (link[i] == str('/')[0]):
+			filename = ""
+		filename += link[i]
+
+	url = link[len(host) :]
+	if (str(filename) == str(host)):
+		filename = "/"
+	print("[PARSE REQUEST HEADER] HOSTNAME IS", host ,'\n[PARSE REQUEST HEADER] URL IS ',url[1:] ,'\n[PARSE REQUEST HEADER] FILENAME IS ',filename[1:])
+	return host , url[1:] , filename[2:]
+
+header_mp = {}
+file_mp = {}
+message_mp = {}
+while True:
+	clientSocket, address = serverTCP.accept()
+	print("Connection from {} established! Begin client communication." .format(address))
+	#threading.Thread(target=requestsGET, args=(clientSocket, address))
+	data = clientSocket.recv(4096)
+	data = str(data.decode("utf-8"))
+
+	method, link, version = parse_data(data)
+	if (str(link) == str("favicon.ico")):
+		continue
+	print("MESSAGE RECEIVED FROM CLIENT:")
+	print(data)
+	print("")
+	print("END OF MESSAGE RECEIVED FROM CLIENT")
+	
+	print("")
+	print("[PARSE MESSAGE HEADER]")
+	print(' METHOD = ',method,' DESTADDRESS = ',link,' HTTPVersion = ',version)
+
+
+	if (link not in header_mp.keys()):
+		print("[LOOK UP IN THE CACHE]: NOT FOUND, BUILD REQUEST TO SEND TO ORIGINAL SERVER")
+
+
+		target_port = 80  # create a socket object 
+		client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  
+	 
+		# connect the client 
+		 
+		print("REQUEST MESSAGE SENT TO ORIGINAL SERVER")
+		target_host, obj , filename = parse_link(link)	
+		#send some data 
+		request = "GET /" +obj+" HTTP/1.1\r\nHost: " + target_host+"\r\n"
+		request += "Connection: close\r\n"
+		request += "Upgrade-Insecure-Requests: 1\r\n"
+		request += "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36\r\n"
+		request += "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9\r\n"
+		request += "ec-Fetch-Mode: navigate\r\n"
+		request += "Accept-Encoding: gzip, deflate, br\r\n"
+		request += "Accept-Language: en-US,en;q=0.9\r\n"
+		request += "\r\n"
+		#create a socket and send request to sever
+		
+		print(request + "\n")
+
+		print("END OF MESSGA SENT TO ORIGINAL SERVER")
+		client.connect((target_host,target_port))  
+		client.send(request.encode())
+		
+		print("\n\nRESPONSE HEADER FROM ORIGINAL SERVER:")
+		res = client.recv(4096)
+		header_sv = get_header(str(res.decode("utf-8")))
+		print(header_sv)
+
+		print("END OF HEADER\n\n")
+
+		
+		print("RESPONSE HEADER FROM PROXY TO CLIENT:")
+		print(header_sv)
+		clientSocket.send(res)
+		print("END OF HEADER")
+		print("\n\n[WRITE FILE INTO CACHE]: cache/",filename)
+		header_mp[link] = header_sv
+		file_mp[link] = "cache/"+filename
+		message_mp[link] = res
+	else:
+		print("[LOOK UP IN THE CACHE]: FOUND IN THE CACHE: FILE =",file_mp[link])
+		res = message_mp[link]
+		header_sv = header_mp[link]
+		print("RESPONSE HEADER FROM PROXY TO CLIENT:")
+		print(header_sv)
+		clientSocket.send(res)
+		print("END OF HEADER")
