11d25dd2c1c740a417cf242d8181f5d9a9e36e8c
dtzeta259 <catalystoftime259@gmail.com>
2022-04-29 15:04:17 -0500

    small changes

--- WebProxyServer.py
+++ WebProxyServer.py
@@ -121,9 +121,10 @@ while True:
 		request += "Upgrade-Insecure-Requests: 1\r\n"
 		request += "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36\r\n"
 		request += "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9\r\n"
-		request += "ec-Fetch-Mode: navigate\r\n"
+		request += "Sec-Fetch-Site: none\r\n"
+		request += "Sec-Fetch-Mode: navigate\r\n"
 		request += "Accept-Encoding: gzip, deflate, br\r\n"
-		request += "Accept-Language: en-US,en;q=0.9\r\n"
+		request += "Accept-Language: en-US,en;q=0.9,fr;q=0.8,vi;q=0.7\r\n"
 		request += "\r\n"
 		#create a socket and send request to sever
 		
